45bbf2e4dbebc2c298d443082efc77cc22348bbf
David Wetter <david@wetter.sg>
2023-11-14 22:35:06 +0100

    Create Swift Projekt

---
+++ Adoreify-ios/Adoreify_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Adoreify_iosApp.swift
+//  Adoreify-ios
+//
+//  Created by David Wetter on 14.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct Adoreify_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
